19ec8b1ae4b7e848675218765d887bdcd5399994
Elvis de Aguiar e Silva <>
2019-06-03 23:07:39 -0300

    Configurações Iniciais Mockadas
    * Configurações Iniciais Mockadas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend-farmacia-elvis';
+export class AppComponent implements OnInit {
+  vendasPorPeriodo: Array<any> = [];
+
+  vendasPorCategoria: Array<any> = [];
+
+  vendasPorProduto: Array<any> = [];
+
+  ngOnInit() {
+    this.loadReports();
+  }
+
+  loadReports() {
+    this.vendasPorPeriodo = [
+        {
+          'ano': 2019,
+          'mes': 6,
+          'valorVendido': 185.88
+        }
+    ];
+
+    this.vendasPorCategoria = [
+      {
+        'categoria': 'M',
+        'valorVendido': 20.22
+      },
+      {
+        'categoria': 'NM',
+        'valorVendido': 82.80
+      },
+      {
+        'categoria': 'NMA',
+        'valorVendido': 82.86
+      }
+    ];
+
+    this.vendasPorProduto = [
+      {
+        'codigoProduto': 2,
+        'quantidade': 4
+      },
+      {
+        'codigoProduto': 1,
+        'quantidade': 2
+      },
+      {
+        'codigoProduto': 3,
+        'quantidade': 1
+      }
+    ];
+  }
 }
